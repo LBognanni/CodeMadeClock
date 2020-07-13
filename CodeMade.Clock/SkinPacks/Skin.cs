ee7fbfdeddca0481fd30d5458b57671f41f93bcd
Loris Bognanni <loris.bognanni@gmail.com>
2020-07-13 11:26:25 +0100

    small changes
     - Move VariableReplacingFileReader to own file
     - Add .editorconfig
     - Start frmSettings

--- CodeMade.Clock/SkinPacks/Skin.cs
+++ CodeMade.Clock/SkinPacks/Skin.cs
@@ -1,9 +1,6 @@
 ﻿using CodeMade.ScriptedGraphics;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text.RegularExpressions;
 
 namespace CodeMade.Clock.SkinPacks
 {
@@ -25,7 +22,7 @@ namespace CodeMade.Clock.SkinPacks
 
         internal void Load(IFileReader fileReader)
         {
-            if(!string.IsNullOrEmpty(Definition))
+            if (!string.IsNullOrEmpty(Definition))
             {
                 if (fileReader.FileExists(Definition))
                 {
@@ -33,38 +30,5 @@ namespace CodeMade.Clock.SkinPacks
                 }
             }
         }
-
-        class VariableReplacingFileReader : IFileReader
-        {
-            private readonly IFileReader _inner;
-            private readonly IDictionary<string, object> _variables;
-
-            public VariableReplacingFileReader(IFileReader inner, IDictionary<string, object> variables)
-            {
-                _inner = inner;
-                _variables = variables;
-            }
-
-            public bool FileExists(string fileName) => 
-                _inner.FileExists(fileName);
-
-            public string GetFontFile(string fontFile) =>
-                _inner.GetFontFile(fontFile);
-
-            public string GetString(string fileName)
-            {
-                var regexes = _variables.Select(v => new { Regex = new Regex($"\\$\\b{v.Key}\\b"), Replace = v.Value }).ToList();
-                var json = _inner.GetString(fileName);
-                foreach(var regex in regexes)
-                {
-                    json = regex.Regex.Replace(json, regex.Replace.ToString());
-                }
-
-                return json;
-            }
-
-            public Image LoadImage(string path) =>
-                _inner.LoadImage(path);
-        }
-    }
+    }    
 }
---
+++ CodeMade.Clock/SkinPacks/VariableReplacingFileReader.cs
@@ -0,0 +1,42 @@
+﻿using CodeMade.ScriptedGraphics;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeMade.Clock.SkinPacks
+{
+    class VariableReplacingFileReader : IFileReader
+    {
+        private readonly IFileReader _inner;
+        private readonly IDictionary<string, object> _variables;
+
+        public VariableReplacingFileReader(IFileReader inner, IDictionary<string, object> variables)
+        {
+            _inner = inner;
+            _variables = variables;
+        }
+
+        public bool FileExists(string fileName) => 
+            _inner.FileExists(fileName);
+
+        public string GetFontFile(string fontFile) =>
+            _inner.GetFontFile(fontFile);
+
+        public string GetString(string fileName)
+        {
+            var regexes = _variables.Select(v => new { Regex = new Regex($"\\$\\b{v.Key}\\b"), Replace = v.Value }).ToList();
+            var json = _inner.GetString(fileName);
+            foreach(var regex in regexes)
+            {
+                json = regex.Regex.Replace(json, regex.Replace.ToString());
+            }
+
+            return json;
+        }
+
+        public Image LoadImage(string path) =>
+            _inner.LoadImage(path);
+    }
+    
+}
