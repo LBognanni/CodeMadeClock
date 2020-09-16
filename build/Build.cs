1ccf242e938eea2e93ca41811eb99feafb3b769a
Loris Bognanni <loris.bognanni@gmail.com>
2020-09-16 23:08:04 +0100

    Default to release

--- build/Build.cs
+++ build/Build.cs
@@ -28,8 +28,8 @@ class Build : NukeBuild
 
     public static int Main () => Execute<Build>(x => x.Compile);
 
-    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+    [Parameter("Configuration to build - Default is 'Release'")]
+    readonly Configuration Configuration = Configuration.Release;
 
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
