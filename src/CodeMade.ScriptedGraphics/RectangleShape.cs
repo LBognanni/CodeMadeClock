43d2408198eabc0820f1d5317616a382c546708f
Loris Bognanni <loris@codemade.co.uk>
2024-04-21 13:53:24 +0100

    :sparkles: add CornerRadius to RectangleShape

--- src/CodeMade.ScriptedGraphics/RectangleShape.cs
+++ src/CodeMade.ScriptedGraphics/RectangleShape.cs
@@ -1,5 +1,6 @@
 ﻿using CodeMade.ScriptedGraphics.Colors;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 
 namespace CodeMade.ScriptedGraphics
 {
@@ -58,20 +59,51 @@ namespace CodeMade.ScriptedGraphics
             Color = color;
         }
 
+        /// <summary>
+        /// The radius of the corners of the rectangle. If set to 0, the rectangle will have sharp corners.
+        /// If set to a value greater than 0, the rectangle will have rounded corners, and the "Smooth" option will also implicitly be true.
+        /// </summary>
+        public float CornerRadius { get; set; } = 0;
+
+        private GraphicsPath GetPath(float scaleFactor)
+        {
+            var path = new GraphicsPath();
+            path.AddArc(Left * scaleFactor, Top * scaleFactor, CornerRadius * scaleFactor, CornerRadius * scaleFactor, 180, 90);
+            path.AddArc((Left + Width - CornerRadius) * scaleFactor, Top * scaleFactor, CornerRadius * scaleFactor, CornerRadius * scaleFactor, 270, 90);
+            path.AddArc((Left + Width - CornerRadius) * scaleFactor, (Top + Height - CornerRadius) * scaleFactor, CornerRadius * scaleFactor, CornerRadius * scaleFactor, 0, 90);
+            path.AddArc(Left * scaleFactor, (Top + Height - CornerRadius) * scaleFactor, CornerRadius * scaleFactor, CornerRadius * scaleFactor, 90, 90);
+            path.CloseFigure();
+            return path;
+        }
+
         public virtual void Render(Graphics g, float scaleFactor)
         {
-            RectangleF rect = new RectangleF(Left * scaleFactor, Top * scaleFactor, Width * scaleFactor, Height * scaleFactor);
+            var rect = new RectangleF(Left * scaleFactor, Top * scaleFactor, Width * scaleFactor, Height * scaleFactor);
+            var slightlyBiggerRect = new RectangleF(Left * scaleFactor, Top * scaleFactor, Width * scaleFactor * 1.1f, Height * scaleFactor * 1.1f);
 
-            using (var smoothing = new SmoothingOption(g, Smooth? System.Drawing.Drawing2D.SmoothingMode.AntiAlias : System.Drawing.Drawing2D.SmoothingMode.Default))
+            using (var smoothing = new SmoothingOption(g, Smooth || (CornerRadius > 0) ? SmoothingMode.AntiAlias : SmoothingMode.Default))
             {
-                var brushes = Color.ParseBrush(rect);
+                var brushes = Color.ParseBrush(Smooth || (CornerRadius > 0) ? slightlyBiggerRect : rect);
                 try
                 {
-                    foreach (var brush in brushes)
+                    if (CornerRadius == 0)
                     {
-                        brush.Match(
-                            b => g.FillRectangle(b, rect),
-                            r => g.FillRegion(r.Color, r.Region));
+                        foreach (var brush in brushes)
+                        {
+                            brush.Match(
+                                b => g.FillRectangle(b, rect),
+                                r => g.FillRegion(r.Color, r.Region));
+                        }
+                    }
+                    else
+                    {
+                        var path = GetPath(scaleFactor);
+                        foreach (var brush in brushes)
+                        {
+                            brush.Match(
+                                b => g.FillPath(b, path),
+                                r => g.FillRegion(r.Color, r.Region));
+                        }
                     }
                 }
                 finally
